31d99730e8697781ea6be46efafac2df80b04896
Fumitaka Hijino <Hijino@hijino-no-macbook-puro-2017.local>
2018-02-03 11:16:28 +0900

    6.1 地面をスクロールさせる

--- FlappyBird/GameScene.swift
+++ FlappyBird/GameScene.swift
@@ -10,26 +10,53 @@ import SpriteKit
 
 class GameScene: SKScene {
     
+    var scrollNode:SKNode!
+    
     // SKView上にシーンが表示されたときに呼ばれるメソッド
     override func didMove(to view: SKView) {
         // 背景色を設定
         backgroundColor = UIColor(red:0.15, green:0.75, blue:0.90, alpha:1)
         
+        // スクロールするスプライトの親ノード
+        scrollNode = SKNode()
+        addChild(scrollNode)
+        
         // 地面の画像を読み込む
         let groundTexture = SKTexture(imageNamed: "ground")
         groundTexture.filteringMode = .nearest
         
-        // テクスチャを指定してスプライトを作成する
-        let groundSprite = SKSpriteNode(texture: groundTexture)
+        // 必要な枚数を計算
+        let needNumber = Int(self.frame.size.width / groundTexture.size().width) + 2
         
-        // スプライトの表示する位置を指定する
-        groundSprite.position = CGPoint(
-            x: groundTexture.size().width * 0.5,
-            y: groundTexture.size().height * 0.5
-        )
+        // スクロールするアクションを作成
+        // 左方向に画像一枚分スクロールさせるアクション
+        let moveGround = SKAction.moveBy(x: -groundTexture.size().width, y: 0, duration: 5.0)
         
-        // シーンにスプライトを追加する
-        addChild(groundSprite)
+        // 元の位置に戻すアクション
+        let resetGround = SKAction.moveBy(x: groundTexture.size().width, y: 0, duration: 0.0)
+        
+        // 左にスクロール->元の位置->左にスクロールと無限に繰り替えるアクション
+        let repeatScrollGround = SKAction.repeatForever(SKAction.sequence([moveGround, resetGround]))
+        
+        
+        // groundのスプライトを配置する
+        for i in 0..<needNumber {
+            let sprite = SKSpriteNode(texture: groundTexture)
+            
+            // スプライトの表示する位置を指定する
+            sprite.position = CGPoint(
+                x: groundTexture.size().width * (CGFloat(i) + 0.5),
+                y: groundTexture.size().height * 0.5
+            )
+            
+            // スプライトにアクションを設定する
+            sprite.run(repeatScrollGround)
+            
+            // スプライトを追加する
+            scrollNode.addChild(sprite)
+        }
+        
+        
         
     }
 }
